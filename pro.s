74d1c8038a7f672856a79aaa2b0812850a63b418
JSGC <162055263+JSGC-ECI@users.noreply.github.com>
2025-07-25 17:29:06 -0500

    Update pro.s

--- pro.s
+++ pro.s
@@ -1,133 +1,86 @@
-#include <stdio.h>
-#include <string.h>
-
-#define MAX 1005
-
-void quitarCeros(char num[]) {
-    int i = 0;
-    while (num[i] == '0' && num[i + 1] != '\0') {
-        i++;
-    }
-    if (i > 0) {
-        int j = 0;
-        while (num[i]) {
-            num[j++] = num[i++];
-        }
-        num[j] = '\0';
-    }
-}
-
-int comparar(char a[], char b[]) {
-
-    int la = strlen(a);
-    int lb = strlen(b);
-    if (la > lb) return 1;
-    if (la < lb) return -1;
-
-    for (int i = 0; i < la; i++) {
-        if (a[i] > b[i]) return 1;
-        if (a[i] < b[i]) return -1;
-    }
-
-    return 0;
-}
-
-void complementoA9(char b[], char res[]) {
-    int len = strlen(b);
-    for (int i = 0; i < len; i++) {
-        res[i] = '9' - b[i] + '0';
-    }
-    res[len] = '\0';
-}
-
-void sumaConAcarreo(char a[], char b[], char res[]) {
-    int la = strlen(a);
-    int lb = strlen(b);
-    int max = (la > lb) ? la : lb;
-    char temp[MAX];
-    int acarreo = 1;
-    int i = la - 1, j = lb - 1, k = 0;
-
-    while (max--) {
-        int da = (i >= 0) ? a[i] - '0' : 0;
-        int db = (j >= 0) ? b[j] - '0' : 0;
-        int suma = da + db + acarreo;
-
-        acarreo = suma / 10;
-        temp[k++] = (suma % 10) + '0';
-        i--; j--;
-    }
-
-    if (acarreo == 1)
-        temp[k++] = '1';
+.global _start
 
 
-    for (int m = 0; m < k; m++) {
-        res[m] = temp[k - 1 - m];
-    }
-    res[k] = '\0';
-}
+
+.section .data
+    prompt_cases: .asciz "Numero de casos: "
+    prompt_input: .asciz "Ingrese numero: "
+    output_fmt:   .asciz "Maximo valor: %d\n"
+
+    num_cases: .word 2          @ Número de casos de prueba
+    inputs:    .word 4, 7       @ Ejemplo de entrada: 4 y 7
+
+.section .bss
+    num: .skip 4
+    max: .skip 4
 
 
-void restarComplemento9(char a[], char b[], char res[]) {
-    char b9[MAX], suma[MAX];
-    int lenA = strlen(a), lenB = strlen(b);
+.section .text
+_start:
+    ldr r4, =inputs        @ r4 apunta al arreglo de entradas
+    ldr r5, =num_cases     @ r5 tiene la dirección del número de casos
+    ldr r6, [r5]           @ r6 = cantidad de casos
+    mov r7, #0             @ r7 = contador de casos
 
-    char bPadded[MAX];
-    int dif = lenA - lenB;
-    for (int i = 0; i < dif; i++) bPadded[i] = '0';
-    for (int i = 0; i < lenB; i++) bPadded[dif + i] = b[i];
-    bPadded[lenA] = '\0';
 
-    complementoA9(bPadded, b9);
-    sumaConAcarreo(a, b9, suma);
 
-    strcpy(res, suma + 1);
-}
 
-void dividir(char a[], char b[], char cociente[], char residuo[]) {
-    char actual[MAX], temp[MAX], uno[] = "1";
-    int len = strlen(a);
-    int ci = 0;
 
-    char parte[MAX] = "";
 
-    for (int i = 0; i < len; i++) {
-        int l = strlen(parte);
-        parte[l] = a[i];
-        parte[l + 1] = '\0';
-        quitarCeros(parte);
 
-        int cuenta = 0;
-        while (comparar(parte, b) >= 0) {
-            restarComplemento9(parte, b, temp);
-            strcpy(parte, temp);
-            cuenta++;
-        }
 
-        cociente[ci++] = cuenta + '0';
-    }
 
-    cociente[ci] = '\0';
-    quitarCeros(cociente);
+loop_cases:
+    cmp r7, r6             @ si r7 == casos, salir
+    beq end_program
 
-    if (strlen(parte) == 0)
-        strcpy(residuo, "0");
-    else
-        strcpy(residuo, parte);
-}
 
-void main() {
-    int T;
-    char a[MAX], b[MAX], coc[MAX], res[MAX];
-    scanf("%d", &T);
-    for (int i = 0; i < T; i++) {
-        scanf("%s", a);
-        scanf("%s", b);
 
-        dividir(a, b, coc, res);
 
-        printf("%s\n", coc);
-        printf("%s\n", res);
-    }
-}
+
+
+
+
+
+
+
+
+    ldr r0, [r4, r7, LSL #2] @ cargar input[r7] en r0
+    mov r1, r0             @ r1 = num actual
+    mov r2, r0             @ r2 = max actual
+
+collatz_loop:
+    cmp r1, #1
+    beq print_max          @ si num == 1, terminamos esta secuencia
+
+    and r3, r1, #1         @ r3 = r1 % 2
+    cmp r3, #0
+    beq is_even            @ si es par
+    b   is_odd             @ si es impar
+
+is_even:
+    mov r1, r1, LSR #1     @ dividir entre 2 usando shift
+    b   update_max
+
+is_odd:
+    mov r3, r1, LSL #1     @ r3 = 2*num
+    add r1, r3, r1         @ r1 = 3*num
+    add r1, r1, #1         @ r1 = 3*num + 1
+
+update_max:
+    cmp r1, r2
+    ble collatz_loop       @ si r1 <= max, no actualiza
+    mov r2, r1             @ si r1 > max, actualiza max
+    b   collatz_loop
+
+print_max:
+    ldr r0, =output_fmt
+    mov r1, r2
+    bl printf              @ imprime el máximo de la secuencia
+
+    add r7, r7, #1         @ contador++
+    b loop_cases
+
+end_program:
+    mov r7, #1             @ syscall exit (Linux)
+    svc 0
